647075424be4cccd8071b62660e91168eeefe886
DeniMartinez <denimartinez298@gmail.com>
2023-05-14 11:45:06 -0600

    comenzando

---
+++ app/src/main/java/com/example/bachesapp/ExportarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bachesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExportarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exportar)
+    }
+}
---
+++ app/src/main/java/com/example/bachesapp/NubeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bachesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NubeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nube)
+    }
+}
